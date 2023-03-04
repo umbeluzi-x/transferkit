9462da6b9f709aeb04bfef2966ec133bb308c269
Edson Michaque <edson@michaque.com>
2023-03-04 14:40:28 +0200

    Add transaction status

--- transaction.go
+++ transaction.go
@@ -1,7 +1,22 @@
 package providerkit
 
+type TransactionStatus string
+
+const (
+	TransactionStatusSuccess    = TransactionStatus("SUCCESS")
+	TransactionStatusUnknown    = TransactionStatus("UNKNOWN")
+	TransactionStatusProcessing = TransactionStatus("PROCESSING")
+	TransactionStatusFailure    = TransactionStatus("FAILURE")
+)
+
+type TransactionFailureReason string
+
+const (
+	TransactionFailureNotEnoughBalance = TransactionFailureReason("NOT_ENOUGH_BALANCE")
+	TransactionFailureInvalidAccount   = TransactionFailureReason("INVALID_ACCOUNT")
+)
 
 type Transaction struct {
-	ID int64 `json:"id"`
+	ID     int64             `json:"id"`
+	Status TransactionStatus `json:"status"`
 }
-
